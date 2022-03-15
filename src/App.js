2b10dff4db878f6110962e75f55bd56985d074cd
kawgh1 <krseven@gmail.com>
2022-03-15 02:04:26 -0500

    setup components, navbar and routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
+import React from "react";
 import "./App.css";
+// no switch in React Router 6, now called Switches
+import { Routes, Route, Navigate } from "react-router-dom";
+// components
+import Navbar from "../src/components/navbar/Navbar";
+import ContactList from "./components/contacts/ContactList";
+import AddContact from "./components/contacts/AddContact";
+import EditContact from "./components/contacts/EditContact";
+import ViewContact from "./components/contacts/ViewContact";
 
-function App() {
+const App = () => {
     return (
-        <div className="App">
-            <button className="btn btn-success">
-                <i className="fa fa-home"></i>hello
-            </button>
+        <div>
+            <Navbar />
+            <Routes>
+                <Route
+                    path={"/"}
+                    element={<Navigate to={"/contacts/list"} />}
+                />
+                <Route path={"/contacts/list"} element={<ContactList />} />
+                <Route path={"/contacts/add"} element={<AddContact />} />
+                <Route
+                    path={"/contacts/view/:contactId"}
+                    element={<ViewContact />}
+                />
+                <Route
+                    path={"/contacts/edit/:contactId"}
+                    element={<EditContact />}
+                />
+            </Routes>
         </div>
     );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
 // Fontawesome icons
 import "../node_modules/@fortawesome/fontawesome-free/css/all.css";
@@ -12,7 +13,9 @@ import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Router>
+            <App />
+        </Router>
     </React.StrictMode>,
     document.getElementById("root")
 );
